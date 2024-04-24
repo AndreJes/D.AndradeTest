60a05cc494ced5c7e9a5b3442db2169e0faebddc
Andre Jesus <andrej.silva@outlook.com>
2024-04-24 10:12:02 -0300

    Mensagens log

--- D.AndradeTest/Program.cs
+++ D.AndradeTest/Program.cs
@@ -12,22 +12,31 @@ namespace D.AndradeTest
             var lecturesManager = LectureManager.Instance;
             var trackManager = TrackManager.Instance;
 
+            Console.WriteLine($"Lendo arquivo de entrada...");
             var rawLectures = FileHandler.ReadFileLines(@".\input.txt");
 
+            Console.WriteLine($"Obtendo dados do arquivo...");
             var lectures = lecturesManager.ParseLectures(rawLectures);
 
             Console.WriteLine($"Total de Palestras encontradas: {lectures.Count}");
 
+            Console.WriteLine($"Gerando trilhas: {lectures.Count}");
             var tracks = trackManager.GenerateTracks(lectures);
 
             var totalTracks = tracks.Count();
 
-            Console.WriteLine($"Trilhas: {totalTracks}");
+            Console.WriteLine($"Total de trilhas: {totalTracks}");
 
             for (int i = 0; i < totalTracks; i++)
             {
+
+                Console.WriteLine($"=====================================================================================");
                 Console.WriteLine($"[Trilha {i+1}]\n{tracks.ElementAt(i)}\n");
             }
+            Console.WriteLine($"=====================================================================================");
+
+            Console.WriteLine($"Pressione qualquer tecla para finalizar...");
+            Console.ReadLine();
         }
     }
 }
